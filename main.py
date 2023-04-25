f5c681a63d422ee3f263e579242a85bcaa350b94
ZhPaganel <95427867+ZhPaganel@users.noreply.github.com>
2023-04-25 18:58:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,6 @@ import Bio
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 
-'''# intro question
-intro = input('Do you want the program to generate a NA sequence or paste your own one? Paste/Generate ')
-
-# defining variables
-gen_code_dna = ['a', 't', 'g', 'c']
-gen_code_rna = ['a', 'u', 'g', 'c']
-seq = []
-
-if intro == 'Generate' or intro == 'generate':
-    nc_type = input("Please, enter the type of nucleic acid: ")
-    nc_length = int(input("Please, enter the size of the sequence: "))
-    if nc_type == "DNA" or nc_type == "dna":
-        seq = random.choices(gen_code_dna, k=nc_length)
-    elif nc_type == "RNA" or nc_type == "rna":
-        seq = random.choices(gen_code_rna, k=nc_length)
-    new_seq = ''.join(seq)
-else:
-    new_seq = input('Please, paste your sequence in FASTA format: ')
-
-print(new_seq)
-'''
-
 file = open('seq.fasta', 'w')
 fasta = file.write(new_seq)
 file.close()
